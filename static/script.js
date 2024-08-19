0de18dc39a24d500e0addb60a7d488340a6bfda0
Tommaso Cerruti <tommaso.cerruti@casavo.com>
2024-08-20 01:50:26 +0200

    fix: issue #2

--- static/script.js
+++ static/script.js
@@ -43,7 +43,8 @@ document.addEventListener('DOMContentLoaded', function () {
             },
             options: {
                 responsive: false,
-                maintainAspectRatio: true,
+                maintainAspectRatio: false,
+                animation: false,
             }
         });
     }
